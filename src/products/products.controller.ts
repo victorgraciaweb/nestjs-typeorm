f8ffaa0835f243157821f1d921de466c2d7096b4
Víctor Gracia Magallón <info@victorgraciaweb.com>
2024-10-29 21:50:28 +0200

    Remove Auth for test

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,49 +1,30 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, Query, ParseUUIDPipe } from '@nestjs/common';
 
-import { PaginationDto } from 'src/common/dto/pagination.dto';
-import { GetUser } from 'src/auth/decorators/get-user.decorator';
-import { ValidRoles } from 'src/auth/interfaces';
-
 import { ProductsService } from './products.service';
 import { CreateProductDto, UpdateProductDto } from './dto';
-import { Auth } from 'src/auth/decorators/auth.decorator';
-import { User } from 'src/auth/entities/user.entity';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) { }
 
   @Post()
-  @Auth(ValidRoles.user)
-  create(
-    @GetUser() user: User,
-    @Body() createProductDto: CreateProductDto
-  ) {
+  create(@Body() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
 
   @Get()
-  @Auth()
-  findAll(
-    @GetUser() user: User,
-    @Query() paginationDto: PaginationDto
-  ) {
+  findAll(@Query() paginationDto: PaginationDto) {
     return this.productsService.findAll(paginationDto);
   }
 
   @Get(':term')
-  @Auth()
-  findOne(
-    @GetUser() user: User,
-    @Param('term') term: string
-  ) {
+  findOne(@Param('term') term: string) {
     return this.productsService.findOnePlain(term);
   }
 
   @Patch(':id')
-  @Auth(ValidRoles.admin)
   update(
-    @GetUser() user: User,
     @Param('id', ParseUUIDPipe) id: string,
     @Body() updateProductDto: UpdateProductDto
   ) {
@@ -51,11 +32,7 @@ export class ProductsController {
   }
 
   @Delete(':id')
-  @Auth(ValidRoles.admin)
-  remove(
-    @GetUser() user: User,
-    @Param('id', ParseUUIDPipe 
-  ) id: string) {
+  remove(@Param('id', ParseUUIDPipe) id: string) {
     return this.productsService.remove(id);
   }
 }
