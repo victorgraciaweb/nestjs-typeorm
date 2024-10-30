db905292c905ff207fc40d002ae5a241b17d369c
Víctor Gracia Magallón <info@victorgraciaweb.com>
2024-10-30 09:57:00 +0200

    Password encrypted

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@nestjs/common';
-import { ProductsService } from 'src/products/products.service';
-import { initialData } from './data/seed-data';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+
+import * as bcrypt from 'bcrypt';
+
 import { User } from 'src/auth/entities/user.entity';
+import { ProductsService } from 'src/products/products.service';
+import { initialData } from './data/seed-data';
 
 @Injectable()
 export class SeedService {
@@ -36,14 +39,20 @@ export class SeedService {
 
   private async insertUsers() {
     const seedUsers = initialData.users;
-    
+
     const users: User[] = [];
 
-    seedUsers.forEach( user => {
-      users.push( this.userRepository.create( user ) )
+    seedUsers.forEach(user => {
+      const { password, ...userData } = user;
+      users.push(
+        this.userRepository.create({
+          ...userData,
+          password: bcrypt.hashSync(password, 10)
+        })
+      )
     });
 
-    const dbUsers = await this.userRepository.save( seedUsers )
+    const dbUsers = await this.userRepository.save(users);
 
     return dbUsers[0];
   }
@@ -53,8 +62,8 @@ export class SeedService {
 
     const insertPromises = [];
 
-    products.forEach( product => {
-      insertPromises.push( this.productsService.create( product, user ) );
+    products.forEach(product => {
+      insertPromises.push(this.productsService.create(product, user));
     });
 
     await Promise.all(insertPromises);
